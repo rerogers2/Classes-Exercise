f557b4dcd75dd6b8cee7e86ca80298f1ce4578ef
Robert Rogers <rerogers@gmail.com>
2023-03-13 19:54:10 -0500

    Updated exercise 1 for classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var newCar = new Car();
+            newCar.Make = "Honda";
+            newCar.Model = "CRV";
+            newCar.Year = 2019;
+
+            newCar.Convo();
         }
     }
 }
